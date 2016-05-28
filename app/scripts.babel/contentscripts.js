a93d568548cec76672cc60e09e38acff717e4908
Leko <leko.noor@gmail.com>
2016-05-29 01:20:04 +0900

    initial commit

---
+++ app/scripts.babel/contentscripts.js
@@ -0,0 +1,10 @@
+
+// import ChatworkEvents from 'ext-chatwork-events'
+
+// ChatworkEvents.on('ready', () => {
+//   const e = document.createEvent('MouseEvent')
+//   e.initEvent('click', true, true)
+//   e.eventName = e.eventType
+//   document.getElementById('_addButton').dispatchEvent(e)
+//   console.log('クリックしたどん')
+// })
